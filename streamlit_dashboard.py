65917c76bb3d892555d1536bd66b7db421a2ff90
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-05-25 14:38:34 +0700

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -104,7 +104,7 @@ with open('LightGBM_smote_tuned.pckl', 'rb') as f:
     model = pickle.load(f)
 
 # Memuat dataframe dari dashboard
-df_feature_importance = pd.read_csv('df_feature_importance_25.csv')
+df_feature_importance = pd.read_csv('df_feature_importance_25_01.csv')
 df_feature_importance.drop('Unnamed: 0', axis=1, inplace=True)
 df_dashboard_final = pd.read_csv('df_dashboard_final.csv')
 df_dashboard_final.drop('Unnamed: 0', axis=1, inplace=True)
